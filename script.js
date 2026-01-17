b126811f40ffdd5a34be2faba5ea02426c329201
levoweal <69198520+levoweal@users.noreply.github.com>
2026-01-17 19:00:32 +0400

    Finish all intended functionality, clear out bugs

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const display = document.querySelector(".display");
 const numBtns = document.querySelectorAll(".number");
 const operBtns = document.querySelectorAll(".operator");
-const equals = document.querySelector(".equals");
+const equalsBtn = document.querySelector(".equals");
+const clearBtn = document.querySelector(".clear");
+const backspaceBtn = document.querySelector('.backspace');
 
 let num1 = '';
 let num2 = '';
@@ -38,37 +40,70 @@ operBtns.forEach(operBtn => {
             operator = operBtn.value;
             display.textContent = `${num1} ${operator}`
         } else if (operator === '/' && num2) {
-            num1 = Number(num1) / Number(num2);
-            num2 = '';
-            operator = operBtn.value;
-            display.textContent = `${num1} ${operator}`
-        } else {
+            if (num2 === '0') {
+                alert("Can't do that :)");
+                num2 = '';
+                operator = operBtn.value;
+                display.textContent = `${num1} ${operator}`
+            } else {
+                num1 = +(Number(num1) / Number(num2)).toFixed(2);
+                num2 = '';
+                operator = operBtn.value;
+                display.textContent = `${num1} ${operator}`
+            }
+        } else if (num1) {
             operator = operBtn.value;
             display.textContent = `${num1} ${operator}`;
         }
     })
 })
 
-equals.addEventListener('click', () => {
+equalsBtn.addEventListener('click', () => {
     if (operator === '+' && num2) {
-        num1 = `${Number(num1) + Number(num2)}`;
+        display.textContent = `${Number(num1) + Number(num2)}`;
+        num1 = '';
         num2 = '';
         operator = '';
-        display.textContent = `${num1}`;
     } else if (operator === '-' && num2) {
-        num1 = `${Number(num1) - Number(num2)}`;
+        display.textContent = `${Number(num1) - Number(num2)}`;
+        num1 = '';
         num2 = '';
         operator = '';
-        display.textContent = `${num1}`;
     } else if (operator === '*' && num2) {
-        num1 = `${Number(num1) * Number(num2)}`;
+        display.textContent = `${Number(num1) * Number(num2)}`;
+        num1 = '';
         num2 = '';
+        operator = '';
+    } else if (operator === '/' && num2) {
+        if (num2 === '0') {
+            alert("Can't do that :)");
+            num2 = '';
+            display.textContent = `${num1} ${operator}`
+        } else {
+            display.textContent = `${+(Number(num1) / Number(num2)).toFixed(2)}`;
+            num1 = '';
+            num2 = '';
+            operator = '';
+        }
+    }
+})
+
+clearBtn.addEventListener('click', () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    display.textContent = '0';
+})
+
+backspaceBtn.addEventListener('click', () => {
+    if (num2) {
+        num2 = num2.slice(0, -1);
+        display.textContent = `${num1} ${operator} ${num2}`;
+    } else if (operator) {
         operator = '';
         display.textContent = `${num1}`;
-    } else if (operator === '/' && num2) {
-        num1 = `${Number(num1) / Number(num2)}`;
-        num2 = '';
-        operator = '';
+    } else if (num1) {
+        num1 = num1.slice(0, -1);
         display.textContent = `${num1}`;
     }
 })
