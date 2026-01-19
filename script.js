edf443a8071ee06644072c126d549217940efdd5
levoweal <69198520+levoweal@users.noreply.github.com>
2026-01-20 01:08:40 +0400

    Fix more bugs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const display = document.querySelector(".display");
 const numBtns = document.querySelectorAll(".number");
+const dotBtn = document.querySelector('.dot');
 const operBtns = document.querySelectorAll(".operator");
 const equalsBtn = document.querySelector(".equals");
 const clearBtn = document.querySelector(".clear");
@@ -22,6 +23,16 @@ numBtns.forEach(numBtn => {
     })
 });
 
+dotBtn.addEventListener('click', () => {
+    if (operator && num2 !== "" && !num2.includes(".")) {
+        num2 += dotBtn.value;
+        display.textContent = `${num1} ${operator} ${num2}`;
+    } else if (!num2 && num1 !== "" && !num1.includes(".")) {
+        num1 += dotBtn.value; 
+        display.textContent = `${num1}`;
+    }
+})
+
 operBtns.forEach(operBtn => {
     operBtn.addEventListener('click', () => {
         if (operator === '+' && num2) {
@@ -35,12 +46,12 @@ operBtns.forEach(operBtn => {
             operator = operBtn.value;
             display.textContent = `${num1} ${operator}`
         } else if (operator === '*' && num2) {
-            num1 = `${Number(num1) * Number(num2)}`;
+            num1 = `${+(Number(num1) * Number(num2)).toFixed(2)}`;
             num2 = '';
             operator = operBtn.value;
             display.textContent = `${num1} ${operator}`
         } else if (operator === '/' && num2) {
-            if (num2 === '0') {
+            if (+num2 === 0) {
                 alert("Can't do that :)");
                 num2 = '';
                 operator = operBtn.value;
@@ -70,12 +81,12 @@ equalsBtn.addEventListener('click', () => {
         num2 = '';
         operator = '';
     } else if (operator === '*' && num2) {
-        display.textContent = `${Number(num1) * Number(num2)}`;
+        display.textContent = `${+(Number(num1) * Number(num2)).toFixed(2)}`;
         num1 = '';
         num2 = '';
         operator = '';
     } else if (operator === '/' && num2) {
-        if (num2 === '0') {
+        if (+num2 === 0) {
             alert("Can't do that :)");
             num2 = '';
             display.textContent = `${num1} ${operator}`
